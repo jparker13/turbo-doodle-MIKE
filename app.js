b6b730291dd3c2d0514e75c181c13a539cb1db3e
Janna Beth Parker <jannaparker@MacBook-Pro-3.local>
2024-01-24 20:21:53 -0600

    added new requirment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5500;
 
 app.get('/', function (req, res) {
-    res.send('Hello World from Express')
+    res.send('Hello World from Express \n My name is Janna Parker and I cannot find a fully free ennagram test...\n I have answered atleast 100 questions and they all tried to make me pay before i could see my results \n SO... here are my own answers...\n I will alwasy do my part in the group and then some\n I will call you out for not doing your group work.')
 })
 
 //app.listen(3000)
